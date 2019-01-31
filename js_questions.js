4026201de83d7ccf6337aa85646348b594461230
juan0melendez <jmelendez2+code@cub.uca.edu>
2019-01-31 16:56:33 -0500

    got the alert for question 8 to work

--- js_questions.js
+++ js_questions.js
@@ -121,9 +121,22 @@ document.getElementsByName("skills")[0]
 // NOTE: Write unobtrusive Javascript
 // NOTE: This is regarding the favoriteColor radio buttons.
 // When a user selects a color, create an alert with a message similar to:
-// 	"So you like green more than blue now?"
+// 	"So you like green more than blue now?
 // In this example, green is the new value and blue is the old value.
 // Make the background color (of all favoriteColor radio buttons) the newly selected favoriteColor
+function colorEventFactory() {
+  let color = "";
+  return (event) => {
+    color && alert("so you like "+event.target.value+" more than "+color);
+    color = event.target.value;
+  }
+}
+
+let colorRadioButtons = document.getElementsByName("favoriteColor");
+let colorListener = colorEventFactory();
+for(rb of colorRadioButtons) {
+  rb.addEventListener("click", colorListener)
+}
 
 // 9. Show/Hide Event
 // NOTE: Write unobtrusive Javascript
